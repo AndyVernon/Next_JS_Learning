0b8d86210ce21ae837a83aef6c3bb2fdc9a41b91
andyvernon <andy_vernon@live.co.uk>
2023-11-25 16:21:17 +0000

    Starting adding styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,21 +2,23 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script';
 
+import Layout from '../../src/common/_layout'
+
 export default function FirstPost() {
     return (
-        <>
-        <Head>
-            <title>First Post</title>
-        </Head>
-        <Script 
-            src='https://connect.facebook.net/en-US/sdk.js' 
-            strategy='lazyOnload'
-            onLoad={() => console.log('script loaded correctly ')} />
-        <h1>First Post</h1>
-        <h2>
-            <Link href="/">Back</Link>
-        </h2>
-        </>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script 
+                src='https://connect.facebook.net/en-US/sdk.js' 
+                strategy='lazyOnload'
+                onLoad={() => console.log('script loaded correctly ')} />
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back</Link>
+            </h2>
+        </Layout>
     );
 
 }
